377da2ab31e6f4b8bb03ac8b56f0060eb550b5dc
wesley1248 <wesleytms.es@gmail.com>
2023-08-16 20:03:54 -0300

    up

---
+++ verificadorDePalavraPalindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verificadorDePalavraPalindrome {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma palavra para verificarmos se ela é um Palíndromo");
+            string palavra = Console.ReadLine();
+
+            char[] inversaoEtransformacao = palavra.ToCharArray();
+
+            Array.Reverse(inversaoEtransformacao);
+
+            string verificacao = new string(inversaoEtransformacao);
+
+            if (verificacao == palavra)
+            {
+                Console.WriteLine("a palavra é um palíndromo");
+            }
+            else
+            {
+                Console.WriteLine("a palavra não é um palíndromo");
+            }
+        }
+    }
+}
